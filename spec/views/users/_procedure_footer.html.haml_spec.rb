82db2f13e2dfd08e81689ef48a7bd1e9bf20bca3
simon lehericey <mail@simon.lehericey.net>
2026-09-24 09:05:39 +0200

    fix(procedure): check the scheme of the lien_dpo link before rendering it
    The footer link goes through allowed_uri?, the check of the rails sanitizer,
    like the other links rendered from stored content.

--- spec/views/users/_procedure_footer.html.haml_spec.rb
+++ spec/views/users/_procedure_footer.html.haml_spec.rb
@@ -88,5 +88,34 @@ describe 'users/procedure_footer', type: :view do
       before { dossier.procedure.update(lien_dpo: 'https://beta.gouv.fr') }
       it { is_expected.to have_link('Contacter le Délégué à la Protection des Données', href: 'https://beta.gouv.fr') }
     end
+
+    context "when a javascript link was stored before validation rejected it" do
+      before { dossier.procedure.update_column(:lien_dpo, 'javascript:alert(window.location)//@almond.eu') }
+      it { is_expected.not_to have_text('Contacter le Délégué à la Protection des Données') }
+    end
+
+    ['JaVaScRiPt:alert(1)//@x.fr', 'vbscript:msgbox(1)//@x.fr', 'data:text/html,<script>alert(1)</script>@x.fr', 'javascript://x@x.fr/%0Aalert(1)'].each do |lien_dpo|
+      context "when #{lien_dpo.inspect} was stored before validation rejected it" do
+        before { dossier.procedure.update_column(:lien_dpo, lien_dpo) }
+        it { is_expected.not_to have_text('Contacter le Délégué à la Protection des Données') }
+      end
+    end
+
+    context "when the scheme hides in an entity" do
+      before { dossier.procedure.update_column(:lien_dpo, 'javascript&#58;alert(1)//@x.fr') }
+      it { is_expected.to have_link('Contacter le Délégué à la Protection des Données', href: '//javascript&#58;alert(1)//@x.fr') }
+    end
+
+    ['" onmouseover="alert(1)" x="@x.fr', '"><script>alert(1)</script>@x.fr', 'https://x.fr/" onmouseover="alert(1)'].each do |lien_dpo|
+      context "when #{lien_dpo.inspect} tries to break out of the href" do
+        before { dossier.procedure.update_column(:lien_dpo, lien_dpo) }
+
+        it "keeps it inside the href" do
+          is_expected.to have_link('Contacter le Délégué à la Protection des Données')
+          is_expected.not_to have_css('[onmouseover]')
+          is_expected.not_to have_css('script', visible: :all)
+        end
+      end
+    end
   end
 end
